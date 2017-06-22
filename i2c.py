63b5c79db18ba43d0045f9b5cca1febca22d8297
hidewon <hidewon@gmail.com>
2017-06-22 21:21:15 +0900

    ADT7410 is ...

--- i2c.py
+++ i2c.py
@@ -8,8 +8,8 @@ i2c=pi.I2C()
 temp_dev=i2c.setup(0x48)
 
 i2c.writeReg8(temp_dev,0x03,0x80)
-# ADT7410 is broken(because of short circut )
-# But, ADT7410 indicated any temperature.i dont know why.
+# ADT7410 is broken (because of an electrical short circuit )
+# But, ADT7410 indicated any temperature.i dont know what causes an electrical short circuit.
 while True:
 	temp_data=struct.unpack('2B',os.read(temp_dev,2))
 	temp=((temp_data[0] << 8) + temp_data[1])
